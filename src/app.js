c3846b71964287465b0919b75b462247d249db3d
VinTosh-SIR <vintoxen@gmail.com>
2022-07-31 17:40:40 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './app.css';
+import {Box} from "@mui/material";
+
+function App() {
+  return (
+    <div className="app">
+      <Box>
+
+      </Box>
+    </div>
+  );
+}
+
+export default App;
